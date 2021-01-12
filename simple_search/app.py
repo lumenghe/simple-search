2bc21fc190ea317037b1356278f06f46a2612fab
lumenghe <lumenghe@gmail.com>
2021-01-12 07:38:35 +0100

    feat(app): add logger

--- simple_search/app.py
+++ simple_search/app.py
@@ -6,6 +6,10 @@ from collections import Counter
 from typing import Counter as TypeCount
 from typing import Generator
 
+
+logger = logging.getLogger(__name__)
+
+
 class SimpleSearch:
     """class of a command line text search engine."""
 
