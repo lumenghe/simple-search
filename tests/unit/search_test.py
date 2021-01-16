258207fade30bf9b1a6063469ea7b0ae1ceb9303
lumenghe <lumenghe@gmail.com>
2021-01-16 09:05:41 +0100

    test(unit): add test_init

--- tests/unit/search_test.py
+++ tests/unit/search_test.py
@@ -1,3 +1,7 @@
 from unittest.mock import patch
 import pytest
 from simple_search import SimpleSearch
+
+def test_init():
+    simple_search = SimpleSearch()
+    assert simple_search._search_base is None
