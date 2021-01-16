6c4b956e31b76aadf03a865918028db24ceb42fc
lumenghe <lumenghe@gmail.com>
2021-01-16 09:05:59 +0100

    test(unit): add test__build_search_base

--- tests/unit/search_test.py
+++ tests/unit/search_test.py
@@ -5,3 +5,8 @@ from simple_search import SimpleSearch
 def test_init():
     simple_search = SimpleSearch()
     assert simple_search._search_base is None
+
+def test__build_search_base():
+    simple_search = SimpleSearch()
+    simple_search._build_search_base("tests")
+
