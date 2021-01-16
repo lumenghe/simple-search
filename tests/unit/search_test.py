d8227f3a3c746fa6158faabb68acdfefc9937f1f
lumenghe <lumenghe@gmail.com>
2021-01-16 09:07:54 +0100

    test(unit): make style

--- tests/unit/search_test.py
+++ tests/unit/search_test.py
@@ -1,14 +1,19 @@
 from unittest.mock import patch
+
 import pytest
+
 from simple_search import SimpleSearch
+
 
 def test_init():
     simple_search = SimpleSearch()
     assert simple_search._search_base is None
 
+
 def test__build_search_base():
     simple_search = SimpleSearch()
     simple_search._build_search_base("tests")
+
 
 def test_start_path_none():
     simple_search = SimpleSearch()
@@ -16,6 +21,7 @@ def test_start_path_none():
         scores = simple_search.start(None)
         for _ in scores:
             assert True
+
 
 @patch("builtins.input")
 def test_start_path_not_none(input_mock):
