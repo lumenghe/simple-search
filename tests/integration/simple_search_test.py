118d56e088947f521e58ee301235445b3372003a
lumenghe <lumenghe@gmail.com>
2021-01-17 16:45:35 +0100

    test(integration): test_simple_search

--- tests/integration/simple_search_test.py
+++ tests/integration/simple_search_test.py
@@ -1,3 +1,13 @@
 from unittest.mock import patch
 
 from simple_search import SimpleSearch
+
+@patch("builtins.input")
+def test_simple_search(input_mock):
+    input_mock.side_effect = ["def search", ":quit"]
+    search = SimpleSearch()
+    for result in search.start("tests"):
+        assert (
+            result
+            == "tests/integration/simple_search_test.py: 100%\ntests/unit/search_test.py: 50%\n"
+        )
